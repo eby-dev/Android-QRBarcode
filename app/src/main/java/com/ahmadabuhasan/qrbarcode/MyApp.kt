04b467175a1a815b34ad429e1e465f48f30f94a2
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2025-12-19 00:45:05 +0700

    feat: add MyApp class to initialize MobileAds
    This commit introduces the `MyApp` class, extending `Application`, to initialize the Google Mobile Ads SDK upon application startup.

---
+++ app/src/main/java/com/ahmadabuhasan/qrbarcode/MyApp.kt
@@ -0,0 +1,12 @@
+package com.ahmadabuhasan.qrbarcode
+
+import android.app.Application
+import com.google.android.gms.ads.MobileAds
+
+class MyApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        MobileAds.initialize(this)
+    }
+}
